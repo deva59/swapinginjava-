c323b03f291d79e8443e7aec91d47bb5b8d0110b
deva59 <devendrashimpi59@gmail.com>
2024-07-02 10:24:54 +0530

    first Commit

---
+++ swap.java
@@ -0,0 +1,45 @@
+//Swapping two numbers
+//intput a=100 and b=200
+//output a=200 and b=100
+
+
+
+//Using UserDefined...
+
+
+// import java.util.Scanner;
+// class swap{
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+    
+//     System.out.println("Enter A number is ");
+//     int a = sc.nextInt();
+
+//     System.out.println("Enter B number is ");
+//     int b = sc.nextInt();
+
+//     int temp = a;
+//     a = b;
+//     b = temp; 
+
+//     System.out.println("-:After Swapping:-");
+//     System.out.println("1st number:= "+ a);
+//     System.out.println("2nd number:=" + b);
+
+//     sc.close();
+// }
+// }
+
+
+//Using PreDefined...
+
+class java{
+    public static void main(String[] args) {
+        int a =100, b=200;
+        System.out.println("Before Swaping - a= " + a + " And - b= "+ b );
+        int temp  = a;
+        a =b;
+        b=temp;
+        System.out.println("After Swaping - a= "+ a + " And - b= "+ b );
+    }
+}
